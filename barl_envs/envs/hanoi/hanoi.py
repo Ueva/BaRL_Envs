593d6f18be1da9df8651665c1dd6f8156d72a1b9
Joshua Evans <ueva97@gmail.com>
2020-10-16 11:08:08 +0100

    initial method stubs

---
+++ barl_envs/envs/hanoi/hanoi.py
@@ -0,0 +1,27 @@
+import gym
+import numpy as np
+
+from barl_envs.renderers import HanoiRenderer
+
+
+class HanoiEnv(gym.Env):
+    def __init__(self, num_poles, num_discs):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode="human"):
+        pass
+
+    def close(self):
+        pass
+
+    def get_actions(self, state=None):
+        pass
+
+    def get_action_mask(self, state=None):
+        pass
