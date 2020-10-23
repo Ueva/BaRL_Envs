8e1431d0b14a9a11e81dc39228fcef3802af3c6f
Joshua Evans <ueva97@gmail.com>
2020-10-23 11:53:21 +0100

    Finished HanoiEnvironment
    + Added docstrings to all public methods.
    + Added get_successors method.
    + Added is_state_terminal method.
    * Fixed several bugs/typos.

--- barl_envs/envs/hanoi/hanoi.py
+++ barl_envs/envs/hanoi/hanoi.py
@@ -1,4 +1,4 @@
-# Much of this code is adapted from: https://github.com/RobertTLange/gym-hanoi/blob/master/gym_hanoi/envs/hanoi_env.py
+# Much of this code is based on: https://github.com/RobertTLange/gym-hanoi/blob/master/gym_hanoi/envs/hanoi_env.py
 import sys
 import gym
 import math
@@ -14,6 +14,14 @@ class HanoiEnvironment(gym.Env):
     metadata = {"render.modes": ["human"]}
 
     def __init__(self, num_disks=3, num_poles=3):
+        """
+        Instantiates a new HanoiEnvironment object with a specified number
+        of disks and poles.
+
+        Args:
+            num_disks (int, optional): Number of poles in the environment. Defaults to 3.
+            num_poles (int, optional): Number of disks in the environment. Defaults to 3.
+        """
 
         self.num_disks = num_disks
         self.num_poles = num_poles
@@ -22,8 +30,8 @@ class HanoiEnvironment(gym.Env):
         self.action_space = gym.spaces.Discrete(
             math.factorial(self.num_poles) / math.factorial(self.num_poles - 2)
         )
-        self.state_space = gym.tuple(
-            gym.spaces.Tuple(self.num_disks * (gym.spaces.Discrete(self.num_poles),))
+        self.state_space = gym.spaces.Tuple(
+            self.num_disks * (gym.spaces.Discrete(self.num_poles),)
         )
 
         # Initialise action mappings.
@@ -37,11 +45,31 @@ class HanoiEnvironment(gym.Env):
         self.renderer = None
 
     def reset(self):
+        """
+        Resets the environment to an initial state, with all disks stacked
+        on the leftmost pole (i.e. pole with index zero).
+
+        Returns:
+            tuple: Initial environmental state.
+        """
         self.current_state = self.num_disks * (0,)
         self.terminal = False
         return copy.deepcopy(self.current_state)
 
     def step(self, action):
+        """
+        Executes the given action in the environment, causing it to transition
+        to a new state and yield some reward.
+
+        Args:
+            action (int): Index of the action to execute.
+
+        Raises:
+            RuntimeError: Raised when the environment has not been reset at the start of a new episode.
+
+        Returns:
+            next_state (tuple), reward (float), terminal (bool), info (dict)
+        """
         if self.terminal:
             raise RuntimeError("Please call env.reset() before starting a new episode.")
 
@@ -58,7 +86,7 @@ class HanoiEnvironment(gym.Env):
             new_state = tuple(new_state)
         # If the chosen action is illegal, state doesn't change.
         else:
-            info["invalid_action":True]
+            info["invalid_action"] = True
 
         # Reward is 10 for reaching the goal state, -1 otherwise.
         reward = 10 if new_state == self.goal_state else -1
@@ -75,11 +103,26 @@ class HanoiEnvironment(gym.Env):
         pass
 
     def close(self):
+        """
+        Cleanly stops the environment, closing any associated renderer.
+        """
+        # Close renderer, if one exists.
         if self.renderer is not None:
             self.renderer.close()
+            self.renderer = None
 
     def get_available_actions(self, state=None):
-        if state == None:
+        """
+        Returns the list of actions available in the given state.
+        If no state is specified, the actions available in the current state will be returned.
+
+        Args:
+            state (tuple, optional): The state. Defaults to None (i.e. the current state).
+
+        Returns:
+            List[int]: List of actions available in the given state.
+        """
+        if state is None:
             state = self.current_state
 
         legal_actions = []
@@ -90,17 +133,80 @@ class HanoiEnvironment(gym.Env):
         return legal_actions
 
     def get_action_mask(self, state=None):
-        if state == None:
+        """
+        Returns a boolean mask indicating which actions are available in the given state.
+        If no state is specified, an action mask for the current state will be returned.
+
+        A value of True at index i indicates that this action is available.
+        A value of False at index i indicates that the corresponding action is not available.
+
+        Keyword Arguments:
+            state (tuple, optional) -- The state to return an action mask for. Defaults to None (i.e. current state).
+        Returns:
+            list[int]: The list of actions available in the given state.
+        """
+        if state is None:
             state = self.current_state
 
+        # Get legal actions in given state.
         legal_actions = self.get_available_actions(state=state)
+
+        # Get list of all actions.
         all_actions = list(range(len(self.action_list)))
+
+        # True is action is in legal actions, false otherwise.
         legal_action_mask = map(lambda action: action in legal_actions, all_actions)
 
         return list(legal_action_mask)
 
+    def get_successors(self, state=None):
+        """
+        Returns a list of states which can be reached by taking an action in the given state.
+        If no state is specified, a list of successor states for the current state will be returned.
+
+        Args:
+            state (tuple, optional): The state to return successors for. Defaults to None (i.e. current state).
+
+        Returns:
+            list[tuple]: A list of states reachable by taking an action in the given state.
+        """
+        if state is None:
+            state = self.current_state
+
+        new_state = state
+        legal_actions = self.get_available_actions(state=new_state)
+
+        # Creates a list of all states which can be reached by
+        # taking the legal actions available in the given state.
+        successor_states = []
+        for action in legal_actions:
+            successor_state = list(copy.deepcopy(state))
+            source_pole, dest_pole = self.action_list[action]
+            disk_to_move = min(self._disks_on_pole(source_pole, state=new_state))
+            successor_state[disk_to_move] = dest_pole
+            successor_states.append(copy.deepcopy(tuple(successor_state)))
+
+        return successor_states
+
+    def is_state_terminal(self, state=None):
+        """
+        Returns whether the given state is terminal or not.
+        If no state is specified, whether the current state is terminal will be returned.
+
+        Args:
+            state (tuple, optional): Whether or not the given state is terminal. Defaults to None (i.e. current state).
+
+        Returns:
+            bool: Whether or not the given state is terminal.
+        """
+        if state is None:
+            state = self.current_state
+
+        # A state is only terminal if it is the goal state.
+        return state == self.goal_state
+
     def _is_action_legal(self, action, state=None):
-        if state == None:
+        if state is None:
             state = self.current_state
 
         source_pole, dest_pole = action
@@ -120,6 +226,6 @@ class HanoiEnvironment(gym.Env):
                 return min(source_disks) < min(dest_disks)
 
     def _disks_on_pole(self, pole, state=None):
-        if state == None:
+        if state is None:
             state = self.current_state
         return [disk for disk in range(self.num_disks) if state[disk] == pole]
