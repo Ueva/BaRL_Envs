77e4e5d7d576eee2d9ae9aab2d65eab3b1cc9926
Joshua Evans <ueva97@gmail.com>
2020-12-07 11:40:19 +0000

    Updated setup.py and initialised grid_pacman environment

---
+++ barl_envs/envs/grid_pacman/__init__.py
@@ -0,0 +1,3 @@
+from barl_envs.envs.grid_pacman.grid_pacman import GridPacManEnvironment
+from barl_envs.envs.grid_pacman.grid_pacman import PacManFourRoom
+from barl_envs.envs.grid_pacman.grid_pacman import PacManClassic
---
+++ barl_envs/envs/grid_pacman/grid_pacman.py
@@ -0,0 +1,16 @@
+import gym
+import copy
+import numpy as np
+import networkx as nx
+
+
+class GridPacManEnvironment(object):
+    pass
+
+
+class PacManFourRoom(object):
+    pass
+
+
+class PacManClassic(object):
+    pass
