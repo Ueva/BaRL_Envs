8000d8e54afea5ffedffce41997eb4edb498d8fe
Joshua Evans <jbe25@bath.ac.uk>
2024-03-04 18:16:48 +0000

    Added Continuous Rooms Environment

---
+++ simpleenvs/envs/continuous_rooms/rooms.py
@@ -0,0 +1,147 @@
+import math
+import random
+import numpy as np
+import gymnasium as gym
+
+from simpleoptions.function_approximation import GymWrapper
+
+
+CELL_TYPES_DICT = {".": "floor", "#": "wall", "S": "start", "G": "goal", "A": "agent"}
+
+ACTIONS_DICT = {0: "UP", 1: "DOWN", 2: "LEFT", 3: "RIGHT"}
+
+
+class ContinuousRoomsEnvironment(gym.Env):
+
+    metadata = {"render.modes": ["human", "rgb_array", "ascii"]}
+
+    def __init__(self, room_template_file_path, explorable=False):
+        super().__init__()
+
+        self._initialise_rooms(room_template_file_path, explorable)
+
+        self.observation_space = gym.spaces.Box(
+            low=np.array([-10.0, -10.0]), high=np.array([10.0, 10.0]), dtype=np.float32
+        )  # 2D continuous state space.
+        self.action_space = gym.spaces.Discrete(4)  # 4 discrete actions.
+
+    def _initialise_rooms(self, room_template_file_path, explorable):
+        # Load gridworld template file.
+        self.gridworld = np.loadtxt(room_template_file_path, comments="//", dtype=str)
+
+        self.y_cells = self.gridworld.shape[0]
+        self.x_cells = self.gridworld.shape[1]
+
+        # Base Python version (fastest):
+        self.y_interp = lambda y: (y - 0) * (10.0 - -10.0) / (self.y_cells + 1 - 0) + -10.0
+        self.x_interp = lambda x: (x - 0) * (10.0 - -10.0) / (self.x_cells + 1 - 0) + -10.0
+
+        # NumPy version (slower):
+        # self.y_interp = lambda y: np.interp(y, [0, self.y_cells + 1], [-10.0, 10.0])
+        # self.x_interp = lambda x: np.interp(x, [0, self.x_cells + 1], [-10.0, 10.0])
+
+        # SciPy version (slowest):
+        # self.y_interp = scipy.interpolate.interp1d([0, self.y_cells + 1], [-10.0, 10.0])
+        # self.x_interp = scipy.interpolate.interp1d([0, self.x_cells + 1], [-10.0, 10.0])
+
+        # Discover start and goal states.
+        self.initial_states = []
+        self.terminal_states = []
+        for y in range(self.gridworld.shape[0]):
+            for x in range(self.gridworld.shape[1]):
+                if CELL_TYPES_DICT[self.gridworld[y, x]] == "start":
+                    self.initial_states.append((y, x))
+                elif CELL_TYPES_DICT[self.gridworld[y, x]] == "goal" and not explorable:
+                    self.terminal_states.append((y, x))
+
+    def reset(self, state=None):
+        if state is None:
+            initial_grid_square = random.choice(self.initial_states)
+            initial_y = random.uniform(initial_grid_square[0], initial_grid_square[0] + 1)
+            initial_x = random.uniform(initial_grid_square[1], initial_grid_square[1] + 1)
+            self.current_state = (initial_y, initial_x)
+        else:
+            self.current_state = state
+
+        return self._get_obs(), {}
+
+    def step(self, action):
+
+        current_state = self.current_state
+        next_state = self.current_state
+        noise = random.uniform(-0.2, 0.0)
+
+        # Move the agent.
+        if action == 0:  # UP
+            next_state = (current_state[0] - 1 - noise, current_state[1])
+        elif action == 1:  # DOWN
+            next_state = (current_state[0] + 1 + noise, current_state[1])
+        elif action == 2:  # LEFT
+            next_state = (current_state[0], current_state[1] - 1 - noise)
+        elif action == 3:  # RIGHT
+            next_state = (current_state[0], current_state[1] + 1 + noise)
+
+        reward = 0.0
+        terminal = False
+
+        # Determine whether next-state is valid and/or a terminal state.
+        if CELL_TYPES_DICT[self.gridworld[math.floor(next_state[0]), math.floor(next_state[1])]] == "wall":
+            next_state = current_state
+        elif CELL_TYPES_DICT[self.gridworld[math.floor(next_state[0]), math.floor(next_state[1])]] == "goal":
+            reward = 1.0
+            terminal = True
+
+        reward += -0.01
+
+        self.current_state = next_state
+
+        return self._get_obs(), reward, terminal, False, {}
+
+    def close(self):
+        pass
+
+    def _get_obs(self):
+        return np.array([self.y_interp(self.current_state[0]), self.x_interp(self.current_state[1])], dtype=np.float32)
+
+    def _get_cell(self):
+        return np.array([self.current_state[0], self.current_state[1]], dtype=np.int32)
+
+
+if __name__ == "__main__":
+
+    env = ContinuousRoomsEnvironment("simpleenvs/envs/continuous_rooms/data/xu_four_rooms.txt")
+    env = GymWrapper(env)
+
+    # i = 0
+
+    # while i < 1_000_000:
+
+    #     state, _ = env.reset()
+    #     terminal = False
+
+    #     while not terminal:
+    #         action = env.action_space.sample()
+    #         next_state, reward, terminal, _, _ = env.step(action)
+    #         # print("{}:\t{}, {} --> {}".format(i, state, ACTIONS_DICT[action], next_state))
+    #         state = next_state
+    #         i += 1
+
+    #         if i > 1_000_000:
+    #             break
+
+    # env.close()
+
+    for _ in range(100):
+
+        state, _ = env.reset()
+        terminal = False
+
+        i = 0
+        while not terminal:
+            action = env.action_space.sample()
+            next_state, reward, terminal, _, _ = env.step(action)
+            print("{}:\t{}, {} --> {}".format(i, state, ACTIONS_DICT[action], next_state))
+            state = next_state
+            i += 1
+
+    env.close()
