97109f75af02aed5d701c6e3a8a8257881a4a2ed
Joshua Evans <jbe25@bath.ac.uk>
2024-07-15 15:44:19 +0100

    Blacked test_discrete_rooms.py

--- test/test_discrete_rooms.py
+++ test/test_discrete_rooms.py
@@ -4,18 +4,21 @@ import numpy as np
 
 from simpleenvs.envs.discrete_rooms import DiscreteXuFourRooms
 
-def test_reset()
+
+def test_reset():
     env = DiscreteXuFourRooms()
-    
+
     state = env.reset()
     assert state == (2, 2)
 
+
 def test_goal(self):
     env = DiscreteXuFourRooms()
-    
+
     assert (10, 10) in env.terminal_states
+
 
 def test_start(self):
     env = DiscreteXuFourRooms()
-    
-    assert (2,2) in env.terminal_states
+
+    assert (2, 2) in env.terminal_states
