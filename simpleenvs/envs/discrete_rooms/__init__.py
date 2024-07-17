e2994cdcd3d8aabfce2ddd0bc7e94833c76ec343
Joshua Evans <jbe25@bath.ac.uk>
2024-07-17 16:43:36 +0100

    Added __init__.py Imports for Explorable Rooms

--- simpleenvs/envs/discrete_rooms/__init__.py
+++ simpleenvs/envs/discrete_rooms/__init__.py
@@ -21,3 +21,21 @@ from simpleenvs.envs.discrete_rooms.rooms import ParrMaze
 from simpleenvs.envs.discrete_rooms.rooms import ParrMiniMaze
 from simpleenvs.envs.discrete_rooms.rooms import RameshMaze
 from simpleenvs.envs.discrete_rooms.rooms import WidePath
+
+# Explorable Room Layouts.
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableTwoRooms
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableSixRooms
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableNineRooms
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableXuFourRooms
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableBridgeRoom
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableCageRoom
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableEmptyRoom
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableSmallRooms
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableFourRooms
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableFourRoomsHoles
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableMazeRooms
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableSpiralRoom
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableParrMaze
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableParrMiniMaze
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableRameshMaze
+from simpleenvs.envs.discrete_rooms.explorable_rooms import ExplorableWidePath
