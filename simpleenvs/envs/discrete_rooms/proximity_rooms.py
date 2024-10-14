ab6e8e857c4ca0f72c478de7299d25638414b3ec
Tom <tmssmith@gmail.com>
2024-10-14 12:39:47 +0100

    Revert "Modify reward function"
    This reverts commit de296f837c18249eeb4480b36442b38551d0e2e0.

--- simpleenvs/envs/discrete_rooms/proximity_rooms.py
+++ simpleenvs/envs/discrete_rooms/proximity_rooms.py
@@ -130,8 +130,7 @@ class ProximityRoomEnvironment(TransitionMatrixBaseEnvironment):
 
         if terminal:
             self.is_reset = False
-            # reward -= self.distance_penalty_factor * self.distance_to_goal(state)
-            reward += self.distance_penalty_factor / (self.distance_to_goal(state) + 1)
+            reward -= self.distance_penalty_factor * self.distance_to_goal(state)
 
         return next_state, reward, terminal, info
 
